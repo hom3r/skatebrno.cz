9a2617fcc0b6039c17e36f04cf7912749ee56f2e
David Beran <beran3@gmail.com>
2016-01-23 18:00:35 +0100

    experiment

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80;
+// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 80;
+var server_port = 80;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 // viewed at http://localhost:8080
