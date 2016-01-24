844d32118bf152ff46a735c87bdec777bfc4cd8e
David Beran <beran3@gmail.com>
2016-01-24 22:36:16 +0100

    added ribbon

--- gulpfile.js
+++ gulpfile.js
@@ -115,7 +115,7 @@ gulp.task('test', ['coffee-test'], function() {
     return gulp.src('test/*.js', {read: false})
         // gulp-mocha needs filepaths so you can't have any plugins before it
         .pipe(mocha(
-            {reporter: 'nyan'}
+            // {reporter: 'nyan'}
         ));
 });
 
