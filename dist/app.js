9276edeeae1c04cbb8763f576f186a773eeb577f
David Beran <beran3@gmail.com>
2017-09-06 02:04:38 +0200

    prototype with few static non-interactive marks only

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,2 @@
-(function(){console.log("CoffeeScript is OK"),$(document).ready(function(){return console.log("jQuery is working")})}).call(this);
+(function(){var o;console.log("CoffeeScript is OK"),$(document).ready(function(){return console.log("jQuery is working")}),o=function(){var o;return o=new google.maps.Map(document.getElementById("map"),{center:{lat:-34.397,lng:150.644},zoom:8}),console.log("Map function triggered")},console.log("Map function loaded")}).call(this);
 //# sourceMappingURL=app.js.map
--- src/coffee/app.coffee
+++ src/coffee/app.coffee
@@ -2,3 +2,12 @@ console.log "CoffeeScript is OK"
 
 $(document).ready ->
     console.log "jQuery is working"
+
+initMap = () ->
+    map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+    })
+    console.log "Map function triggered"
+
+console.log "Map function loaded"
